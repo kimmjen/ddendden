ef50d604f9e7e0d63a385b18bc9708b9b320f42e
kimmjen <wpals814@gmail.com>
2024-12-18 17:53:32 +0900

    Add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {Layout} from "@/components/layout/Layout";
 import {useLanguageStore} from "@/store/languageStore";
 import React, {useEffect} from "react";
 import './globals.css'
-
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 function LanguageProvider({ children }: { children: React.ReactNode }) {
     const language = useLanguageStore(state => state.language);
 
@@ -21,12 +21,19 @@ export default function RootLayout({
 }>) {
     return (
         <html>
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>뜬뜬 (DDen DDen)</title>
+        </head>
         <body>
         <LanguageProvider>
             <Layout>
                 {children}
+                {/*<SpeedInsights/>*/}
             </Layout>
         </LanguageProvider>
+
         </body>
         </html>
     );
